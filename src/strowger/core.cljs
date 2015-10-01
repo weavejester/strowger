08c72534bedfc6b0891d667e4dec65307b38aef5
James Reeves <jreeves@weavejester.com>
2015-10-01 18:45:57 +0100

    Initial commit

---
+++ src/strowger/core.cljs
@@ -0,0 +1,6 @@
+(ns strowger.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/strowger/core_test.cljs
@@ -0,0 +1,7 @@
+(ns strowger.core-test
+  (:require [cljs.test :refer :all]
+            [strowger.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
