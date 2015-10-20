d8b713c0e9b320bb3e3630a23de173aec7c12ab1
James Reeves <jreeves@weavejester.com>
2015-10-20 03:35:06 +0100

    Add Codox plugin

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122" :scope "provided"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-codox "0.9.0"]]
+  :codox {:language :clojurescript}
   :cljsbuild
   {:builds {:main {:source-paths ["src"]
                    :compiler {:output-to "target/main.js"
