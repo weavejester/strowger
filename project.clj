472f5da871e5fcbd7167372593c38216ca7ddea4
James Reeves <jreeves@weavejester.com>
2015-10-21 21:49:36 +0100

    Add project URL and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject strowger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A ClojureScript library for managing DOM events"
+  :url "https://github.com/weavejester/strowger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
