dce155d8f631fe4bfd15e25422f8dfaf46b1d8f9
James Reeves <jreeves@weavejester.com>
2015-10-21 21:52:47 +0100

    Update ClojureScript version

--- example/project.clj
+++ example/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [strowger "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "1.1.0"]]
   :cljsbuild
--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122" :scope "provided"]]
+                 [org.clojure/clojurescript "1.7.145" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-codox "0.9.0"]]
   :codox {:language :clojurescript
