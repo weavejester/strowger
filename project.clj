97c8260a623aa423c4cbb630af91bbe175f6afc3
James Reeves <jreeves@weavejester.com>
2016-03-28 22:19:18 +0100

    Add :source-uri to Codox options

--- project.clj
+++ project.clj
@@ -7,9 +7,11 @@
                  [org.clojure/clojurescript "1.7.145" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-codox "0.9.4"]]
-  :codox {:language :clojurescript
-          :metadata {:doc/format :markdown}
-          :output-path "doc"}
+  :codox
+  {:language :clojurescript
+   :metadata {:doc/format :markdown}
+   :output-path "doc"
+   :source-uri "http://github.com/weavejester/strowger/blob/{version}/{filepath}#L{line}"}
   :cljsbuild
   {:builds {:main {:source-paths ["src"]
                    :compiler {:output-to "target/main.js"
